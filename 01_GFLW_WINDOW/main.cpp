de3e1d8eaae77784af5f846dfecf286d1e309f4b
chengreg <644076531@qq.com>
2024-09-16 15:55:16 +0800

    事件响应

--- 01_GFLW_WINDOW/main.cpp
+++ 01_GFLW_WINDOW/main.cpp
@@ -3,8 +3,28 @@
 #include <GLFW/glfw3.h>
 
 /*
-目标创建glfw窗体系统
+目标:
+	-创建glfw窗体系统
+	-加入窗体变化的事件回调
 */
+
+// 声明且实现一个窗体变化的回调函数
+void frameBufferSizeCallBack(GLFWwindow* window, int width, int height) {
+	std::cout << "窗体大小变化: width=" << width << ", height=" << height << std::endl;
+}
+
+// 键盘输入的回调函数
+void keyCallBack(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+		std::cout << "按下了:"<< key << std::endl;
+		std::cout << "scancode:" << scancode << std::endl;
+		std::cout << "action:" << action << std::endl;
+		std::cout << "mods:" << mods << std::endl;
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
 
 int main() {
 	//1.初始化glfw基本环境
@@ -24,6 +44,11 @@ int main() {
     }
 	// 设置当前窗体为opengl的绘制舞台
 	glfwMakeContextCurrent(window);
+
+	// 设置窗体变化的回调函数
+	glfwSetFramebufferSizeCallback(window, frameBufferSizeCallBack);
+	// 设置键盘输入的回调函数
+	glfwSetKeyCallback(window, keyCallBack);
 
 	//3.执行窗体循环
 	while (!glfwWindowShouldClose(window))
